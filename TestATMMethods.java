fa3ba653e7908c0fcde975a4904ca7de2fd551ec
Dhanusha1 <87644268+Dhanusha1@users.noreply.github.com>
2021-07-19 19:08:04 +0530

    Update TestATMMethods.java

--- TestATMMethods.java
+++ TestATMMethods.java
@@ -1,11 +1,14 @@
 package com.ust;
-
+//Class to implement the ATMMethods
 public class TestATMMethods {
-
+        
+	//Creating the main method
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//creating the object of ATMMethod class
 		ATMMethods obj = new ATMMethods();
+		//Calling the function of ATMMethod class
 		obj.Operations();
 
 	}
